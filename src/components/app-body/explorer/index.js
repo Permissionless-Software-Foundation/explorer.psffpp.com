6685d5c69a6c8d23d21119a3c5082cad43f9f69c
Daniel Gonzalez <danielhumgon@gmail.com>
2025-04-22 20:11:17 -0400

    feat(pagination): Added pagination functionality

--- src/components/app-body/explorer/index.js
+++ src/components/app-body/explorer/index.js
@@ -1,49 +1,63 @@
 // Global libraries
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { Spinner } from 'react-bootstrap'
+import Pagination from './pagination'
 import axios from 'axios'
 
 // Local libraries
 import ExplorerTable from './table'
 
 const Explorer = ({ appData }) => {
-  const [pins, setPins] = useState([])
-  const [onFetch, setOnFetch] = useState(false)
-  const [success, setSuccess] = useState(false)
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (onFetch) return
-        setOnFetch(true)
-        console.log(appData.serverUrl)
-        console.log(appData)
-        const url = `${appData.serverUrl}/ipfs/pins/1`
-        const response = await axios.get(url)
-        const data = response.data
-        console.log(data)
-        if (!data.success) throw new Error(data.message)
-        const pinsResponse = data.pins
-        setPins(pinsResponse.pins)
-        setSuccess(true)
-        setOnFetch(false)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-        setOnFetch(false)
-        setSuccess(true)
-        setPins([])
-      }
+  const [pins, setPins] = useState([]) // pins data
+  const [onFetch, setOnFetch] = useState(false) // Flag for prevent multiple fetch
+  const [success, setSuccess] = useState(false) // Flag for prevent multiple fetch on component mount
+  const [error, setError] = useState(null) // error message
+  // Fetch data from the server
+  const fetchData = useCallback(async (toPage = 1) => {
+    try {
+      console.log('toPage', toPage)
+      if (onFetch) return
+      setOnFetch(true)
+      // fetch data from the server
+      const url = `${appData.serverUrl}/ipfs/pins/${toPage}`
+      const response = await axios.get(url)
+      const data = response.data
+      if (!data.success) throw new Error(data.message)
+      const pinsResponse = data.pins
+
+      // update the pins state
+      setPins(pinsResponse.pins)
+      setSuccess(true)
+      setOnFetch(false)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+      setOnFetch(false)
+      setSuccess(true)
+      setError(error.message)
+      setPins([])
     }
+  }, [onFetch, appData.serverUrl])
+
+  // Fetch data when the component mounts
+  useEffect(() => {
     if (!onFetch && !success) {
       fetchData()
     }
-  }, [appData, onFetch, success])
+  }, [appData, onFetch, success, fetchData])
 
   return (
     <div>
-      {!onFetch && success && <ExplorerTable pins={pins} appData={appData} />}
+      {success && !error && <Pagination pins={pins} appData={appData} onPageChange={fetchData} onFetch={onFetch} />}
+      {!onFetch && success && !error && <ExplorerTable pins={pins} appData={appData} />}
       {onFetch && (
-        <div className='balance-spinner-container'>
+        <div style={{ height: '70vh' }} className='balance-spinner-container d-flex justify-content-center align-items-center'>
           <Spinner animation='border' />
+        </div>
+      )}
+      {error && !onFetch && <div style={{ color: 'red' }} className='error-message d-flex justify-content-center align-items-center'>{error}</div>}
+      {success && !error && (
+        <div className='d-flex justify-content-center align-items-center'>
+          <span className='text-muted'>No IPFS pins found.</span>
         </div>
       )}
     </div>
